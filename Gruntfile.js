2f11f058cd2a0a0db06db0b5199cd103c34b02b5
ClementBoudon <clement.boudon@gmail.com>
2015-03-19 22:05:16 +0100

    Corrections CSS + AJout inline css + Correction JS CV

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     compass: {                  
-      dist: {                   
+      all: {                   
         options: {              
           config: 'config.rb'
         }
@@ -15,13 +15,29 @@ module.exports = function(grunt) {
         src: ["public/css/reset.css","public/css/main.css","public/css/cv.css","public/css/responsive.css"],
         dest: "public/css/style.css"
       },
+    },
+    processhtml: {
+      all: {
+        files: {
+          'public/index.html': ['public/src_index.html'],
+          'public/cv.html': ['public/src_cv.html']
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['sass/*','public/src_*'],
+        tasks: ['default'],
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['compass','concat_css']);
+  grunt.registerTask('default', ['compass','concat_css','processhtml']);
 
 };
